8e65ea666cfc278f9333aa0fdeec117b600d1e0d
Elisa Renda <elisa.renda@hotmail.fr>
2019-06-17 23:56:46 +0200

    about

--- src/components/About/aboutme.jsx
+++ src/components/About/aboutme.jsx
@@ -1,49 +1,97 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-mdl';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
-class About extends Component {
-	render() {
-		return(
-			<div className="container-body">
-				<Grid style={{maxWidth:'850px', background:'transparent'}} className="container-grid">	
-					<div className="card">
-						<div className="header-bg">
-							<img className="card-picture" src="../assets/img/elisa.jpg" alt=""/>
-						</div>
-						<div className="info-container">
-							<Link className="menu-button"style={{textDecoration: 'none', color: 'rgb(42, 41, 47)'}} to="/">
-								<img className="logo" src='../assets/img/logo.png' alt="logo"/>
-									elisa
-							</Link>
-							<p className="info-resume">Junior Web Developer</p>
-							<div className="center-resume">
-								<p className="text-resume">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-								<img style={{height:'180px'}} className="qrcode" src="../assets/img/QR_CV.png" alt="qr-code" />
-							</div>
-							<div className="mdl-list">
-								<div style={{padding:'0'}} className="mdl-list__item">
-									<ul>
-										<li>
-											<a href="mailto:elisa.renda@hotmail.fr"><FontAwesomeIcon className="fa fa-envelope" icon={faEnvelope} /></a>
-										</li>
-										<li>
-											<a href="https://www.linkedin.com/in/elisa-renda/"><FontAwesomeIcon className="fa fa-linkedin" icon={faLinkedin} /></a>
-										</li>
-										<li>
-											<a href="https://github.com/elisarenda"><FontAwesomeIcon className="fa fa-github" icon={faGithub} /></a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</Grid>
-			</div>					
-		)
- 	}
-}
+import Wrapper from '../UI/Wrapper/Wrapper';
+import Header from '../UI/Header/Header';
+import classes from './About.module.scss';
 
-export default About;
+const about = () => {
+    return (
+            <div className={classes.About}>
+                <div className={classes.TextArea}>
+                    <Header text="About me" />
+					<p className={classes.Journey}>
+                        A highly driven and motivated developer with over three years’ experience in web development.
+                    </p>
+                    <p className={classes.Journey}>
+                        In 2016, I got my first job as a junior frontend developer at{' '}
+                        <a href="https://www.tma.vn/" target="_blank" rel="noopener noreferrer">
+                            TMA Solutions
+                        </a>{' '}
+                        in HCM, Vietnam. I had the chance working with highly experienced partners like Nokia, building
+                        large Single Page Applications for networking.
+                    </p>
+
+                    <p className={classes.Journey}>
+                        While woking at TMA Solution, I was also exposed to backend development with dockerization and
+                        continous delivery.
+                    </p>
+
+                    <p className={classes.Journey}>
+                        Two years later I moved to Can Tho and started to work for{' '}
+                        <a href="https://codeberryschool.com/" target="_blank" rel="noopener noreferrer">
+                            CodeBerry code school
+                        </a>
+                        . Here, we work as a multi-national, 100% remote team, applied Agile development through Scrum framework deliver features to teach people coding.
+                    </p>
+
+                    <p className={classes.Journey}>
+                        I'm currently resigned from work to study and round out my skills to become a fullstack
+                        developer, looking for growth opportunities from a remote team-based environment.
+                    </p>
+                </div>
+                <div className={classes.ImageArea}>
+					{/* <img className="card-picture" src="../assets/img/elisa.jpg" alt=""/> */}
+					<img style={{height:'160px',width: '160px'}} className="qrcode" src="../assets/img/QR_CV.png" alt="qr-code" />
+					Check more about me!
+                </div>
+            </div>
+    );
+};
+
+export default about;
+// class About extends Component {
+// 	render() {
+// 		return(
+// 			<div className="container-body">
+// 				<Grid style={{maxWidth:'850px', background:'transparent'}} className="container-grid">	
+// 					<div className="card">
+// 						<div className="header-bg">
+// 							<img className="card-picture" src="../assets/img/elisa.jpg" alt=""/>
+// 						</div>
+// 						<div className="info-container">
+// 							<Link className="menu-button"style={{textDecoration: 'none', color: 'rgb(42, 41, 47)'}} to="/">
+// 								<img className="logo" src='../assets/img/logo.png' alt="logo"/>
+// 									elisa
+// 							</Link>
+// 							<p className="info-resume">Junior Web Developer</p>
+// 							<div className="center-resume">
+// 								<p className="text-resume">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+// 								<img style={{height:'180px'}} className="qrcode" src="../assets/img/QR_CV.png" alt="qr-code" />
+// 							</div>
+// 							<div className="mdl-list">
+// 								<div style={{padding:'0'}} className="mdl-list__item">
+// 									<ul>
+// 										<li>
+// 											<a href="mailto:elisa.renda@hotmail.fr"><FontAwesomeIcon className="fa fa-envelope" icon={faEnvelope} /></a>
+// 										</li>
+// 										<li>
+// 											<a href="https://www.linkedin.com/in/elisa-renda/"><FontAwesomeIcon className="fa fa-linkedin" icon={faLinkedin} /></a>
+// 										</li>
+// 										<li>
+// 											<a href="https://github.com/elisarenda"><FontAwesomeIcon className="fa fa-github" icon={faGithub} /></a>
+// 										</li>
+// 									</ul>
+// 								</div>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</Grid>
+// 			</div>					
+// 		)
+//  	}
+// }
+
+// export default About;
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,19 +2,20 @@ import React from 'react';
 import classes from './Skills.module.scss';
 import TagCanvas from '../UI/TagCanvas/TagCanvas';
 import { Link } from 'react-router-dom';
+import Header from '../UI/Header/Header';
 
 const skills = () => {
     return (
             <div className={classes.Skills}>
                 <div className={classes.TextArea}>
-
+				<Header text="Skills / Experiences" />
                     <p className={classes.Journey}>
                         Main area of my expertise is front end development and everything related with web development.
                         HTML, CSS, JS (ES5, ES6), building small & medium Web Apps, React.
                     </p>
                     <p className={classes.Journey}>
                         Often I work as a full-stack developer - mainly in Javascript or Node with framework ReactJS,
-                        VueJS
+                        VueJS.
                     </p>
 
                     <p className={classes.Journey}>
