07c63343773c69be3eae89781f1f33f2f63765d1
Elisa Renda <elisa.renda@hotmail.fr>
2019-06-28 11:01:24 +0200

    projects

--- build/precache-manifest.253538e650ffe54f5f3a5d4b08049606.js
+++ build/precache-manifest.253538e650ffe54f5f3a5d4b08049606.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58e672838e89f8e1066c0bbc008b5f52",
+    "revision": "795f81dc42eb5cf8b531320eace71def",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "5c5ca4a1150cd657d0a3",
-    "url": "/static/css/main.130bd600.chunk.css"
+    "revision": "c5ec3d16befa27811aa7",
+    "url": "/static/css/main.35f5f5c5.chunk.css"
   },
   {
     "revision": "844e60fb94f709a239fa",
     "url": "/static/js/2.65d288bc.chunk.js"
   },
   {
-    "revision": "5c5ca4a1150cd657d0a3",
-    "url": "/static/js/main.c112c989.chunk.js"
+    "revision": "c5ec3d16befa27811aa7",
+    "url": "/static/js/main.0f5655cd.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
